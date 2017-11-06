a1f4ce291a04e27ab5c40696aec979b7ad49bbb7
Florian Durrieu <f.durrieu.ext@meetic-corp.com>
2017-11-06 15:34:39 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ~
+sudo apt-get install python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev libcapstone3 libcapstone-dev libffi-dev file
+git clone https://github.com/byt3bl33d3r/mitmf.git
+cd mitmf && git submodule init && git submodule update --recursive
+./setup.sh
+sudo pip2 install --upgrade -r requirements.txt
+
