8d63467f5b8f97f559b66c39f1720d9fb3bd49dc
Florian Durrieu <f.durrieu.ext@meetic-corp.com>
2017-11-08 10:27:11 +0100

    update before install -_-

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd ~
+apt-get update
 apt-get install python-dev python-setuptools libpcap0.8-dev libnetfilter-queue-dev libssl-dev libjpeg-dev libxml2-dev libxslt1-dev libcapstone3 libcapstone-dev libffi-dev file
 git clone https://github.com/byt3bl33d3r/mitmf.git
 cd mitmf && git submodule init && git submodule update --recursive
